e22b30cd3a4da27900a41600cd8d691cec58e1b4
Darren Picard <darrenpicard25@gmail.com>
2019-02-07 19:32:13 +0000

    Added for each to waldo file

--- wheresWaldo.js
+++ wheresWaldo.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function (x,index) {
+    if (x === "Waldo") {
+      found(index);
     }
-  }
+  });
 }
 
 function actionWhenFound(index) {
@@ -12,3 +12,7 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+//array.forEach(callBack... {currentValue, index, array})
+
+
